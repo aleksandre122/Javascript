c58c0637390b3954bc7d140f9807a188cc65beff
aleksandre122 <bagashvili.alex01@mail.ru>
2024-11-16 14:57:52 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let firstname = "ალექსანდრე"
+console.log(firstname)
+
+let age = 23
+console.log(age)
+
+let history = "დაბადებული 2001 წლის 18 იანვარს , რუსეთში"
+console.log(history)
+
+let home = "თურწეთი , სტამბული"
+console.log(home)
+
+let car = "BMW"
+ car = "TOYOTA"
+console.log(car)
+
+let number = 10
+console.log(typeof number)
+let text = "HELLO"
+console.log(typeof text)
+let training = true
+console.log(typeof training)
+let goodbye
+console.log(typeof goodbye)
+let bike = null
+console.log(typeof bike)
